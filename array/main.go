b70cef2845144c9204861f5ac735abdedb5ceffd
rakiyama <ryojiro25@gmail.com>
2021-11-26 22:45:23 +0900

    done array

--- array/main.go
+++ array/main.go
@@ -16,6 +16,41 @@ func main() {
 		numbers := [...]int{99: -1} // specify -> numbets[99] = -1
 		fmt.Println("Numbers :", numbers)
 	}
+	testname("multiple array")
+	{
+		var ThreeD [2][3][2]int
+		fmt.Println("init: ", ThreeD)
+		for i := 0; i < 2; i++ {
+			for j := 0; j < 3; j++ {
+				for k := 0; k < 2; k++ {
+					ThreeD[i][j][k] = (i + 1) * (j + 1) * (k + 1)
+				}
+			}
+		}
+		fmt.Println("after loop: ", ThreeD)
+	}
+	testname("slice")
+	{
+		months := []string{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
+		quarter2 := months[3:6]
+		quarter2Extended := quarter2[:5]
+		quarter2ToEnd := months[3:]
+		fmt.Println(months, len(months), cap(months))
+		fmt.Println(quarter2, len(quarter2), cap(quarter2))
+		fmt.Println(quarter2Extended, len(quarter2Extended), cap(quarter2Extended))
+		fmt.Println(quarter2ToEnd, len(quarter2ToEnd), cap(quarter2ToEnd))
+		months = append(months, "messenger")
+		fmt.Println(months, len(months), cap(months))
+
+		remove := 2
+		copy_months := make([]string, 13)
+		copy(copy_months, months)
+		if remove < len(copy_months) {
+			fmt.Println("copy months:Before", copy_months, "Remove", copy_months[remove])
+			copy_months = append(copy_months[:remove], copy_months[remove+1:]...)
+			fmt.Println("copy months:After ", copy_months)
+		}
+	}
 }
 
 func testname(s string) {
