5c78d1fa117daca8b44bdd623ead12616e197337
rakiyama <ryojiro25@gmail.com>
2021-12-02 20:03:54 +0900

    add another method

--- method/main.go
+++ method/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type triangle struct {
 	size int
@@ -18,9 +21,34 @@ func (s square) perimeter() int {
 	return s.size * 4
 }
 
+func (t *triangle) doubleSize() {
+	t.size *= 2
+}
+
+type upperstring string
+
+func (s upperstring) Upper() string {
+	return strings.ToUpper(string(s))
+}
+
 func main() {
-	t := triangle{3}
-	s := square{4}
-	fmt.Println("Perimeter (triangle): ", t.perimeter())
-	fmt.Println("Perimeter (square): ", s.perimeter())
+	testname("base method")
+	{
+		t := triangle{3}
+		s := square{4}
+		fmt.Println("Perimeter (triangle): ", t.perimeter())
+		fmt.Println("Perimeter (square): ", s.perimeter())
+		t.doubleSize()
+		fmt.Println("Size: (triangle): ", t.size)
+	}
+	testname("make method another type")
+	{
+		s := upperstring("Learning Go!")
+		fmt.Println(s)
+		fmt.Println(s.Upper())
+	}
+}
+
+func testname(s string) {
+	fmt.Println("---------------[", s, "]")
 }
