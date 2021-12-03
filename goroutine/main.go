fd40833e0b6cb1a67e3561b16086d69835c0880b
rakiyama <ryojiro25@gmail.com>
2021-12-03 23:52:00 +0900

    start goroutine_exercise

--- goroutine/main.go
+++ goroutine/main.go
@@ -15,9 +15,9 @@ func checkAPI(api string, ch chan string) {
 	ch <- fmt.Sprintf("SUCCESS: %s is up and running!\n", api)
 }
 
-func main() {
+func test1() {
+	testname("base goroutine apis")
 	start := time.Now()
-	ch := make(chan string)
 	apis := []string{
 		"https://management.azure.com",
 		"https://dev.azure.com",
@@ -26,6 +26,9 @@ func main() {
 		"https://api.somewhereintheinternet.com/",
 		"https://graph.microsoft.com",
 	}
+	//ch := make(chan string)
+	ch := make(chan string, 10)
+
 	for _, api := range apis {
 		//checkAPI(api)
 		go checkAPI(api, ch)
@@ -36,4 +39,77 @@ func main() {
 	//time.Sleep(3 * time.Second)
 	elapsed := time.Since(start)
 	fmt.Printf("Done! It took %v seconds!\n", elapsed.Seconds())
+}
+
+func send(ch chan string, message string) {
+	ch <- message
+}
+
+func test2() {
+	testname("channel buffer")
+	size := 2
+	ch := make(chan string, size)
+	send(ch, "one")
+	send(ch, "two")
+	go send(ch, "three")
+	go send(ch, "four")
+	fmt.Println("All data sent to the channel ...")
+
+	for i := 0; i < 4; i++ {
+		fmt.Println(<-ch)
+	}
+	fmt.Println("Done!")
+}
+
+func write(ch chan<- string, message string) {
+	fmt.Printf("Writing: %#v\n", message)
+	ch <- message
+}
+
+func read(ch <-chan string) {
+	fmt.Printf("Receiving: %#v\n", <-ch)
+}
+
+func test3() {
+	testname("channel direction")
+	ch := make(chan string, 1)
+	write(ch, "Hello World!")
+	read(ch)
+}
+
+func process(ch chan string) {
+	time.Sleep(3 * time.Second)
+	ch <- "Done processing!"
+}
+
+func replicate(ch chan string) {
+	time.Sleep(1 * time.Second)
+	ch <- "Done replicating!"
+}
+
+func test4() {
+	testname("select")
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	go process(ch1)
+	go replicate(ch2)
+	for i := 0; i < 2; i++ {
+		select {
+		case process := <-ch1:
+			fmt.Println(process)
+		case replicate := <-ch2:
+			fmt.Println(replicate)
+		}
+	}
+}
+
+func main() {
+	//test1()
+	//test2()
+	//test3()
+	test4()
+}
+
+func testname(s string) {
+	fmt.Println("---------------[", s, "]")
 }
