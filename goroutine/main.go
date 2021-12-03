7c011864e5bc545954043a474c5c781f9df4d060
rakiyama <ryojiro25@gmail.com>
2021-12-03 19:57:31 +0900

    add channel

---
+++ goroutine/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func checkAPI(api string, ch chan string) {
+	_, err := http.Get(api)
+	if err != nil {
+		ch <- fmt.Sprintf("ERROR: %s is down!\n", api)
+		return
+	}
+	ch <- fmt.Sprintf("SUCCESS: %s is up and running!\n", api)
+}
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	apis := []string{
+		"https://management.azure.com",
+		"https://dev.azure.com",
+		"https://api.github.com",
+		"https://outlook.office.com/",
+		"https://api.somewhereintheinternet.com/",
+		"https://graph.microsoft.com",
+	}
+	for _, api := range apis {
+		//checkAPI(api)
+		go checkAPI(api, ch)
+	}
+	for i := 0; i < len(apis); i++ {
+		fmt.Print(<-ch)
+	}
+	//time.Sleep(3 * time.Second)
+	elapsed := time.Since(start)
+	fmt.Printf("Done! It took %v seconds!\n", elapsed.Seconds())
+}
