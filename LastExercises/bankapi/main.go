804363804d707284c76ba6ff24ec685ccc525268
rakiyama <ryojiro25@gmail.com>
2021-12-05 16:08:07 +0900

    done

--- LastExercises/bankapi/main.go
+++ LastExercises/bankapi/main.go
@@ -86,6 +86,40 @@ func withdraw(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func send(w http.ResponseWriter, req *http.Request) {
+	sourcenumberqs := req.URL.Query().Get("sourcenumber")
+	remitternumberqs := req.URL.Query().Get("remitternumber")
+	amountqs := req.URL.Query().Get("amount")
+
+	if sourcenumberqs == "" || remitternumberqs == "" {
+		fmt.Fprintf(w, "Account number is missing!")
+		return
+	}
+
+	if sourcenumber, err := strconv.ParseFloat(sourcenumberqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid source account number!")
+	} else if remitternumber, err := strconv.ParseFloat(remitternumberqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid remitter account number!")
+	} else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid amount number!")
+	} else {
+		if sourceaccount, exist := accounts[sourcenumber]; !exist {
+			fmt.Fprintf(w, "Source account with number %v can't be found!", sourcenumber)
+		} else if remitteraccount, exist := accounts[remitternumber]; !exist {
+			fmt.Fprintf(w, "Remitter account with number %v can't be found!", remitternumber)
+		} else {
+			err := sourceaccount.Send(amount, remitteraccount)
+			if err != nil {
+				fmt.Fprintf(w, "%v", err)
+			} else {
+				fmt.Fprintf(w, sourceaccount.Statement())
+				fmt.Println()
+				fmt.Fprintf(w, remitteraccount.Statement())
+			}
+		}
+	}
+}
+
 func main() {
 	accounts[1001] = &bank.Account{
 		Customer: bank.Customer{
@@ -95,9 +129,17 @@ func main() {
 		},
 		Number: 1001,
 	}
-
+	accounts[1002] = &bank.Account{
+		Customer: bank.Customer{
+			Name:    "Ben",
+			Address: "Zushi, Kanagawa",
+			Phone:   "(222) 555 5555",
+		},
+		Number: 1002,
+	}
 	http.HandleFunc("/statement", statement)
 	http.HandleFunc("/deposit", deposit)
 	http.HandleFunc("/withdraw", withdraw)
+	http.HandleFunc("/send", send)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
