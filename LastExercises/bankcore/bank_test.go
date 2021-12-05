9652464f9e9d96973379d62c32284ceed7dfd756
rakiyama <ryojiro25@gmail.com>
2021-12-05 15:15:42 +0900

    make bankcore

--- LastExercises/bankcore/bank_test.go
+++ LastExercises/bankcore/bank_test.go
@@ -1,6 +1,8 @@
 package bank
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestAccount(t *testing.T) {
 	accout := Account{
@@ -15,5 +17,104 @@ func TestAccount(t *testing.T) {
 
 	if accout.Name == "" {
 		t.Error("can't create an Account object")
+	}
+}
+
+func TestDeposit(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name:    "John",
+			Address: "Los Angeles, California",
+			Phone:   "(213) 555 0147",
+		},
+		Number:  1001,
+		Balance: 0,
+	}
+
+	account.Deposit(10)
+
+	if account.Balance != 10 {
+		t.Error("balance is not being updated after a deposit")
+	}
+}
+func TestDepositInvalid(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name:    "John",
+			Address: "Los Angeles, California",
+			Phone:   "(213) 555 0147",
+		},
+		Number:  1001,
+		Balance: 0,
+	}
+
+	if err := account.Deposit(-10); err == nil {
+		t.Error("only positive numbers should be allowed to deposit")
+	}
+}
+
+func TestWithdraw(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name:    "John",
+			Address: "Los Angeles, California",
+			Phone:   "(213) 555 0147",
+		},
+		Number:  1001,
+		Balance: 0,
+	}
+
+	account.Deposit(10)
+	account.Withdraw(10)
+
+	if account.Balance != 0 {
+		t.Error("balance is not being updated after withdraw")
+	}
+}
+
+func TestStatement(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name:    "John",
+			Address: "Los Angeles, California",
+			Phone:   "(213) 555 0147",
+		},
+		Number:  1001,
+		Balance: 0,
+	}
+
+	account.Deposit(100)
+	statement := account.Statement()
+	if statement != "1001 - John - 100" {
+		t.Error("statement doesn't have the proper format")
+	}
+}
+
+func TestSend(t *testing.T) {
+	accountSend := Account{
+		Customer: Customer{
+			Name:    "John",
+			Address: "Los Angeles, California",
+			Phone:   "(213) 555 0147",
+		},
+		Number:  1001,
+		Balance: 100,
+	}
+
+	accountReceive := Account{
+		Customer: Customer{
+			Name:    "Ben",
+			Address: "Zushi, Kanagawa",
+			Phone:   "(222) 555 5555",
+		},
+		Number:  1002,
+		Balance: 0,
+	}
+
+	accountSend.Send(100, &accountReceive)
+	statementSend := accountSend.Statement()
+	statementReceive := accountReceive.Statement()
+	if statementSend != "1001 - John - 0" || statementReceive != "1002 - Ben - 100" {
+		t.Error("statement doesn't have the proper format")
 	}
 }
